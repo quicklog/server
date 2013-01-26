d65b0b6effae5c4ea29c25776351f4d13ad5ac34
James Bloomer <github2@thebloomers.co.uk>
2013-01-26 15:54:47 +0000

    add views for web charts

--- routes/analyse.js
+++ routes/analyse.js
@@ -1,3 +1,14 @@
 exports.getitems = function(req, res) {
-  res.send(200);
+  var data = [
+        {
+          "x": "Procedure 1",
+          "y": 4
+        },
+        {
+          "x": "Procedure 2",
+          "y": 8
+        }
+      ];
+      
+  res.send(data);
 };
