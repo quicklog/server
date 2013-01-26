fbbb4a2dbd8c4c14271035becf812a7683174480
roylines <roy@roylines.co.uk>
2013-01-26 20:40:09 +0000

    implemented get by tag

--- routes/analyse.js
+++ routes/analyse.js
@@ -12,38 +12,13 @@ exports.getitems = function(req, res) {
 };
 
 exports.getitemsbytag = function(req, res) {
-	var tag = req.params.tag;
-
-	var data = [
-        {
-          "x": "2013-01-05",
-          "y": 1
-        },
-        {
-          "x": "2013-01-06",
-          "y": 6
-        },
-        {
-          "x": "2013-01-07",
-          "y": 13
-        },
-        {
-          "x": "2013-01-08",
-          "y": 0
-        },
-        {
-          "x": "2013-01-09",
-          "y": 0
-        },
-        {
-          "x": "2013-01-10",
-          "y": 9
-        },
-        {
-          "x": "2013-01-11",
-          "y": 6
-        }
-    ];
-
+  var who = "THEUSER";
+  var tag = req.params.tag;
+  data.getTagCounts(who, tag, function(e, data) {
+    if(e) {
+      console.error(e);
+      res.send(500);
+    }
     res.send(data);
+  });
 };
