c8f50fbeabf5498f3df402e13cbc2785df9f2553
James Bloomer <github2@thebloomers.co.uk>
2013-02-03 21:34:30 +0000

    refactored some of the javascript

--- public/javascript/quicklog.js
+++ public/javascript/quicklog.js
@@ -1,27 +1,43 @@
 var timechart2;
 
-var getItemForTagForDay = function(tag, day) {
+var clearComments = function() {
+	$('#commentsDate').empty();
+	$('#comments').empty();
+};
+
+var setCommentsDate = function(items) {
+	if(items.length > 0) {
+		var date = moment(parseInt(items[0].timestamp)).format('Do MMMM YYYY');
+		$('#commentsDate').text(date);
+	}
+};
+
+var appendComment = function(time, rating, comment) {
+	$('#comments').append('<tr><td>' + time + '</td><td>' + rating + '</td><td>' + comment + '</td></tr>');
+};
+
+var getRating = function(item) {
+	var rating = '';
+	var starCount = item.rating;
+	
+	while(starCount > 0) {
+		rating = rating + '<i class="icon-star"/></i> ';
+		--starCount;
+	}
+
+	return rating;
+};
+
+var setComments = function(tag, day) {
 	$.getJSON('/api/1/me/items/' + tag + '/' + day + '/', function(items) {
-		$('#commentsDate').empty();
-		$('#comments').empty();
-
-		if(items.length > 0) {
-			var date = moment(parseInt(items[0].timestamp)).format('Do MMMM YYYY');
-			$('#commentsDate').text(date);
-		}
-
-		$.each(items, function(i, item) {
+		clearComments();
+		setCommentsDate(items);
+		
+		items.forEach(function(item, i) {
 			var time = moment(parseInt(item.timestamp)).format('HH:mm:ss');
 			var comment = item.comment;
-
-			var rating = '';
-			var starCount = item.rating;
-			while(starCount > 0) {
-				rating = rating + '<i class="icon-star"/></i> ';
-				--starCount;
-			}
-
-			$('#comments').append('<tr><td>' + time + '</td><td>' + rating + '</td><td>' + comment + '</td></tr>');
+			var rating = getRating(item);
+			appendComment(time, rating, comment);
 		});
 	});
 };
@@ -55,9 +71,8 @@ var createTimeChart = function(tag, data) {
 	            cursor: 'pointer',
 	            events: {
 	                click: function(event) {
-	                    console.log(event.point);
 	                    var time = event.point.x;
-	                    getItemForTagForDay(tag, time);
+	                    setComments(tag, time);
 	                }
 	            }
 	        }
@@ -75,40 +90,45 @@ var createTimeChart = function(tag, data) {
 	});
 };
 
+var setTitle = function(name) {
+	$('#procedure').text(name);
+};
+
+var setTotal = function(items) {
+	var total = _.reduce(
+		items.counts, 
+		function(memo, num) {
+			return memo + num[1];
+		}, 
+		0);
+
+	$('#total').text(total);
+};
+
 var getItemsForTag = function() {
-
 	var tag = $("#tag").val();
-	$('#procedure').text(tag);
-	//$('#average').text('4');
-
-	// Set comments for today
-	getItemForTagForDay(tag, new Date().getTime());
+	setTitle(tag);
+	setComments(tag, new Date().getTime());
 
 	$.getJSON('/api/1/me/analyse/items/' + tag, function(items) {
-
-		var total;
-		total = _.reduce(items.counts, function(memo, num) {
-			return memo + num[1];
-		}, 0);
-
-		$('#total').text(total);
-
+		setTotal(items);
 		createTimeChart(tag, items);
 	});
 
 };
 
-var getItems = function(done) {
+var appendTag = function(tag) {
+	$('#tag').append('<option value="' + tag + '">' + tag + '</option>');
+};
 
+var getItems = function(done) {
 	$.getJSON('/api/1/me/analyse/items', function(items) {
-		$.each(items, function(i, item) {
-			var tag = item.x;
-			$('#tag').append('<option value="' + tag + '">' + tag + '</option>');
+		items.forEach(function(item, i) {
+			appendTag(item.x);
 		});
 
 		done();
 	});
-
 };
 
 var documentReady = function() {
